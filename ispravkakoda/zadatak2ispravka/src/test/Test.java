ef9f2c988ba6eb3c5f4718a9e02032f76d77dd1c
sevarac <sevarac@gmail.com>
2019-01-21 23:23:27 +0100

    paths jars

--- ispravkakoda/zadatak2ispravka/src/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import ispravka_koda.UpisivanjeUTXTFajl4;
-
-public class Test {
-	public static void main(String[] args) {
-		UpisivanjeUTXTFajl4.upisiArmstrongoveBrojeve();
-	}
-}
